4e7d64a0f2ae4edb6acf96fa5ee2eb3bc6d3312d
yimengli <yimengcsu@gmail.com>
2025-07-19 17:39:45 +0200

    cnsia - ch08 - order-service - Add retry logic

--- src/main/java/com/polarbookshop/orderservice/book/BookClient.java
+++ src/main/java/com/polarbookshop/orderservice/book/BookClient.java
@@ -2,7 +2,9 @@ package com.polarbookshop.orderservice.book;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 import reactor.core.publisher.Mono;
+import reactor.util.retry.Retry;
 
 import java.time.Duration;
 
@@ -22,6 +24,11 @@ public class BookClient {
                 .uri(BOOKS_ROOT_API + isbn)
                 .retrieve()
                 .bodyToMono(Book.class) // Returns the retrieved object as Mono<Book>
-                .timeout(Duration.ofSeconds(3)); // Sets a 3 seconds timeout with fallback returns an empty Mono
+                .timeout(Duration.ofSeconds(3)) // Sets a 3 seconds timeout with fallback returns an empty Mono
+                .onErrorResume(WebClientResponseException.NotFound.class,
+                        exception -> Mono.empty()) // Returns an empty object when a 404 response is received
+                .retryWhen(Retry.backoff(3, Duration.ofMillis(100)))
+                .onErrorResume(Exception.class, // If any error happens after the 3 retry attempts, catch the exception and return an empty object
+                        exception -> Mono.empty());
     }
 }
